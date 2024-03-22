3aeb7e71e9c7a64b0fc8dc090f89b8d9461aaa40
Arielson Oliveira <arielson.dev.aws@gmail.com>
2024-03-22 16:42:58 +0000

    infra ready

--- checkout/queue/queue.go
+++ checkout/queue/queue.go
@@ -3,24 +3,35 @@ package queue
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/streadway/amqp"
 )
 
 func Connect() *amqp.Channel {
-	dsn := "amqp://" + os.Getenv("RABBITMQ_DEFAULT_USER") + ":" + os.Getenv("RABBITMQ_DEFAULT_PASS") + "@" + os.Getenv("RABBITMQ_DEFAULT_HOST") + ":" + os.Getenv("RABBITMQ_DEFAULT_PORT") + os.Getenv("RABBITMQ_DEFAULT_VHOST")
-	//dsn := "amqp://rabbitmq:rabbitmq@localhost:5672/"
-	fmt.Println(dsn)
-	conn, err := amqp.Dial(dsn)
-	if err != nil {
-		panic(err.Error())
+	maxRetries := 5
+	retryDelay := 5 * time.Second
+
+	for i := 0; i < maxRetries; i++ {
+		dsn := "amqp://" + os.Getenv("RABBITMQ_DEFAULT_USER") + ":" + os.Getenv("RABBITMQ_DEFAULT_PASS") + "@" + os.Getenv("RABBITMQ_DEFAULT_HOST") + ":" + os.Getenv("RABBITMQ_DEFAULT_PORT") + os.Getenv("RABBITMQ_DEFAULT_VHOST")
+		conn, err := amqp.Dial(dsn)
+		if err != nil {
+			fmt.Printf("Failed to connect to RabbitMQ: %s. Retrying in %s...\n", err.Error(), retryDelay)
+			time.Sleep(retryDelay)
+			continue
+		}
+
+		channel, err := conn.Channel()
+		if err != nil {
+			fmt.Printf("Failed to open a channel: %s. Retrying in %s...\n", err.Error(), retryDelay)
+			time.Sleep(retryDelay)
+			continue
+		}
+
+		return channel
 	}
 
-	channel, err := conn.Channel()
-	if err != nil {
-		panic(err.Error())
-	}
-	return channel
+	panic("Failed to connect to RabbitMQ after multiple attempts")
 }
 
 func Notify(payload []byte, exchange string, routingKey string, ch *amqp.Channel) {
